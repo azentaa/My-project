f3df7e3a2565bb6277e6f05e839da1b193076c58
azentaa <158740961+azentaa@users.noreply.github.com>
2024-02-03 19:19:36 +0700

    1

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public int speed;
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * speed
+            * Input.GetAxis("Mouse X"), 0);
+    }
+}
