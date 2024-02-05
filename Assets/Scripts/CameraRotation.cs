bc18290e2257a9217ab65d00f9eb07a7b7c7ed16
azentaa <158740961+azentaa@users.noreply.github.com>
2024-02-05 14:52:45 +0700

    2

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class CameraRotation : MonoBehaviour
 {
     public int speed;
+    public Transform cameraAxisTransform;
     void Update()
     {
         transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * speed
             * Input.GetAxis("Mouse X"), 0);
+        cameraAxisTransform.transform.localEulerAngles = new Vector3(cameraAxisTransform.transform.localEulerAngles.x 
+                                                                     + Time.deltaTime * speed 
+                                                                     * Input.GetAxis("Mouse Y"), 0, 0);
     }
 }
